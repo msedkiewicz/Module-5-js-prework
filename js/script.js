59a2110f94471f14a9f012240420da4cc6878679
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-11 22:43:15 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ if (playerInput == "1") {
 }
 
 printMessage("Twój ruch to: " + playerMove + ".");
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log("Wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else if (randomNumber == 3) {
+  computerMove = "nożyce";
+} else {
+  computerMove = "Hm, ten ruch jest mi nieznany.";
+}
+
+printMessage("Mój ruch to: " + computerMove + ".");
+

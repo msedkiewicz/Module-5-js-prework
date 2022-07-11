a3ebd752c55981dad80a6137ba3e0313c0d1acfd
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-11 22:42:43 +0200

    Add playerInput

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+
+console.log("Gracz wpisał: " + playerInput);
+
+let playerMove = "nieznany ruch";
+
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == 2) {
+  playerMove = "papier";
+} else if (playerInput == 3) {
+  playerMove = "nożyce";
+} else {
+  playerMove = "Hm, ten ruch jest mi nieznany. Wybierz coś, co ma dla mnie znaczenie!";
+}
+
+printMessage("Twój ruch to: " + playerMove + ".");

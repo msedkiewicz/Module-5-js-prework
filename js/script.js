e8a02e300708c743d5749b947f61c87d5d61e7ef
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-11 22:53:23 +0200

    Add possible results

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ if (playerInput == "1") {
 } else if (playerInput == 3) {
   playerMove = "nożyce";
 } else {
-  playerMove = "Hm, ten ruch jest mi nieznany. Wybierz coś, co ma dla mnie znaczenie!";
+  playerMove =
+    "Hm, ten ruch jest mi nieznany. Wybierz coś, co ma dla mnie znaczenie!";
 }
 
 printMessage("Twój ruch to: " + playerMove + ".");
@@ -36,6 +37,15 @@ if (randomNumber == 1) {
 
 printMessage("Mój ruch to: " + computerMove + ".");
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
+// Możliwe wyniki
+
+if ((computerMove == "kamień" && playerMove == "kamień") || (computerMove == "papier" && playerMove == "papier") || (computerMove == "nożyce" && playerMove == "nożyce")) {
+    printMessage("Remis!");
+} else if ((computerMove == "kamień" && playerMove == "papier") || (computerMove == "nożyce" && playerMove == "kamień") || (computerMove == "papier" && playerMove == "nożyce")) {
+    printMessage("Ty wygrywasz!");
+  }
+  else if ((computerMove == "papier" && playerMove == "kamień") || (computerMove == "kamień" && playerMove == "nożyce") || (computerMove == "nożyce" && playerMove == "papier")) {
+    printMessage("Przegrywasz!");
+  } else {
+    printMessage("Nie mam wystarczającej ilości danych, by roztrzygnąć ten pojedynek :(")
   }

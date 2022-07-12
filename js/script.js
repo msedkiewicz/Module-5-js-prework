4f5c67058ccb7a0807d48ca46b586d3acd9ca4be
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-12 21:18:56 +0200

    Add event listeners to buttons

--- js/script.js
+++ js/script.js
@@ -46,3 +46,14 @@ function playGame(playerInput) {
 
   displayResult(computerMove, playerMove);
 }
+
+document.getElementById("play-rock").addEventListener("click", function () {
+  playGame(1);
+});
+
+document.getElementById("play-paper").addEventListener("click", function () {
+  playGame(2);
+});
+document.getElementById("play-scissors").addEventListener("click", function () {
+  playGame(3);
+});

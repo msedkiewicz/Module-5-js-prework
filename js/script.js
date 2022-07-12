a99193478055753bc894c578da8eb32c4c26cfb7
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-12 21:04:37 +0200

    Add playGame function

--- js/script.js
+++ js/script.js
@@ -1,44 +1,50 @@
-let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+function playGame() {
+  let playerInput = prompt(
+    "Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce."
+  );
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = getMoveName(playerInput);
-let computerMove = getMoveName(randomNumber);
+  let playerMove = getMoveName(playerInput);
+  let computerMove = getMoveName(randomNumber);
 
-function getMoveName(argMoveId) {
-  if (argMoveId == 1) {
-    return "kamień";
-  } else if (argMoveId == 2) {
-    return "papier";
-  } else if (argMoveId == 3) {
-    return "nożyce";
-  } else {
-    printMessage("Nie znam ruchu  " + argMoveId + ".");
-    return "nieznany ruch";
+  function getMoveName(argMoveId) {
+    if (argMoveId == 1) {
+      return "kamień";
+    } else if (argMoveId == 2) {
+      return "papier";
+    } else if (argMoveId == 3) {
+      return "nożyce";
+    } else {
+      printMessage("Nie znam ruchu  " + argMoveId + ".");
+      return "nieznany ruch";
+    }
   }
-}
 
-function displayResult(argComputerMove, argPlayerMove) {
-  printMessage("Zagrałem " + argComputerMove + ", a Ty " + argPlayerMove + ".");
-
-  if (computerMove == playerMove) {
-    printMessage("Remis!");
-  } else if (
-    (computerMove == "kamień" && playerMove == "papier") ||
-    (computerMove == "nożyce" && playerMove == "kamień") ||
-    (computerMove == "papier" && playerMove == "nożyce")
-  ) {
-    printMessage("Ty wygrywasz!");
-  } else if (
-    (computerMove == "papier" && playerMove == "kamień") ||
-    (computerMove == "kamień" && playerMove == "nożyce") ||
-    (computerMove == "nożyce" && playerMove == "papier")
-  ) {
-    printMessage("Przegrywasz!");
-  } else {
+  function displayResult(argComputerMove, argPlayerMove) {
     printMessage(
-      "Nie mam wystarczającej ilości danych, by roztrzygnąć ten pojedynek :("
+      "Zagrałem " + argComputerMove + ", a Ty " + argPlayerMove + "."
     );
-  }
-}
 
-displayResult(computerMove, playerMove);
+    if (computerMove == playerMove) {
+      printMessage("Remis!");
+    } else if (
+      (computerMove == "kamień" && playerMove == "papier") ||
+      (computerMove == "nożyce" && playerMove == "kamień") ||
+      (computerMove == "papier" && playerMove == "nożyce")
+    ) {
+      printMessage("Ty wygrywasz!");
+    } else if (
+      (computerMove == "papier" && playerMove == "kamień") ||
+      (computerMove == "kamień" && playerMove == "nożyce") ||
+      (computerMove == "nożyce" && playerMove == "papier")
+    ) {
+      printMessage("Przegrywasz!");
+    } else {
+      printMessage(
+        "Nie mam wystarczającej ilości danych, by roztrzygnąć ten pojedynek :("
+      );
+    }
+  }
+
+  displayResult(computerMove, playerMove);
+}
